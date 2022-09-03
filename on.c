16f2ef749ea8c93962708d88e2686683a3c0c7c2
ascharlton <71233363+ascharlton@users.noreply.github.com>
2022-09-04 11:19:22 +1200

    Update on.c

--- on.c
+++ on.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
-
 #define	LED	7
 
 int main (void)
 {
-  printf ("Raspberry Pi blink\n") ;
+  printf ("Power Switch...\n") ;
 
   wiringPiSetup () ;
   pinMode (LED, OUTPUT) ;
   {
     digitalWrite (LED, HIGH) ;	// On
-    delay (1000) ;		// mS
+    delay (1000) ;		
     digitalWrite (LED, LOW) ;	// Off
     delay (5000) ;
   }
